a4478db5d01a8b236fe00a5a5ee660aee2def25f
begalve-lopes <begalve.927390438.00@gmail.com>
2026-05-18 12:35:07 +0100

    componetização,paginação e pesquisa

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,11 +1,12 @@
+import './styles.css'
 export const PostCard = ({ post }) => {
     return (
         <div className="post">
             <img src={post.cover} alt={post.title} />
-            <div key={post.id} className="post-content">
-                <h1>{post.title}</h1>
+            <div className="post-content">
+                <h1>{post.title} {post.id}</h1>
                 <p>{post.body}</p>
             </div>
         </div>
-    )
-}
+    );
+};
