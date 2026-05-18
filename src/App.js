8e7fa705d7a4c710b9b32bb9f6a0ef6d4aff7ad2
begalve-lopes <begalve.927390438.00@gmail.com>
2026-05-18 10:56:22 +0100

    aprendo a consumir dados no react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import { PostCard } from "./components/PostCard";
+import { loadPosts } from "./util/load-posts";
+class App extends Component {
+  state = {
+    posts: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+    await this.loadPoststs();
+  }
+
+  loadPoststs = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+   
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <PostCard key={post.id}  post={post} />
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
+export default App;
